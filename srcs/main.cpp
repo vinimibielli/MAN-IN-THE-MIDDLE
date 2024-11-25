27ed1bbce2121107f065be01b8c746e6b96dfe2f
vinimibielli <vinimibielli@gmail.com>
2024-11-25 09:05:59 -0300

    i dont know

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -17,6 +17,10 @@
 #include <sys/ioctl.h>
 #include <netinet/ip_icmp.h>
 #include <sys/time.h>
+#include <linux/if_packet.h>
+#include <net/ethernet.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
 
 #define PORT 9000
 #define BUFFER_SIZE 8192
@@ -29,6 +33,106 @@ void errorFunction(const std::string &message)
     std::cerr << message << std::endl;
     exit(EXIT_FAILURE);
 }
+
+std::string extractHttpUrl(const char *httpData, int httpDataLen)
+{
+    // Check if it's a GET request
+    if (strncmp(httpData, "GET ", 4) == 0)
+    {
+        const char *urlStart = httpData + 4;
+        const char *urlEnd = strstr(urlStart, " ");
+        if (urlEnd != nullptr)
+        {
+            std::string url(urlStart, urlEnd - urlStart);
+            std::cout << "HTTP URL accessed: " << url << std::endl;
+            return url;
+        }
+    }
+    return "";
+}
+
+void receiveMessage(int sockfd)
+{
+    char buffer[BUFFER_SIZE];
+    struct sockaddr_ll receiveAddr;
+    socklen_t addrLen = sizeof(receiveAddr);
+
+    while (true)
+    {
+        int recvLen = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&receiveAddr, &addrLen);
+        if (recvLen < 0)
+        {
+            if (errno == EAGAIN || errno == EWOULDBLOCK)
+            {
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            }
+            else
+            {
+                std::cerr << "Error to receive the message" << std::endl;
+            }
+        }
+        else
+        {
+            //acessing the ethernet header
+            struct ethhdr *eth = (struct ethhdr *)buffer;
+            if (ntohs(eth->h_proto) == ETH_P_IP)
+            {
+                struct iphdr *ip = (struct iphdr *)(buffer + sizeof(struct ethhdr));
+                std::string protocol;
+
+                // Check the protocol
+                switch (ip->protocol)
+                {
+                    case IPPROTO_ICMP:
+                        protocol = "ICMP";
+                        break;
+                    case IPPROTO_TCP:
+                        protocol = "TCP";
+                        break;
+                    case IPPROTO_UDP:
+                        protocol = "UDP";
+                        break;
+                    default:
+                        protocol = "Unknown";
+                        break;
+                }
+
+                std::cout << "Received packet from " << inet_ntoa(*(struct in_addr *)&ip->saddr) << " with protocol: " << protocol << std::endl;
+
+                if (protocol == "TCP")
+                {
+                    struct tcphdr *tcp = (struct tcphdr *)(buffer + sizeof(struct ethhdr) + ip->ihl * 4);
+                    int sourcePort = ntohs(tcp->source);
+                    int destPort = ntohs(tcp->dest);
+                    if (sourcePort == 80 || destPort == 80)
+                    {
+                        std::cout << "HTTP packet detected" << std::endl;
+                        std::string url = extractHttpUrl(buffer + sizeof(struct ethhdr) + ip->ihl * 4 + tcp->doff * 4, recvLen - sizeof(struct ethhdr) - ip->ihl * 4 - tcp->doff * 4);
+                        if (!url.empty())
+                        {
+                            std::cout << "HTTP URL accessed: " << url << std::endl;
+                        }
+                        
+                    }
+                    else if (sourcePort == 443 || destPort == 443)
+                    {
+                        std::cout << "HTTPS packet detected" << std::endl;
+                    }
+                }
+                else if (protocol == "UDP")
+                {
+                    struct udphdr *udp = (struct udphdr *)(buffer + sizeof(struct ethhdr) + ip->ihl * 4);
+                    if (ntohs(udp->source) == 53 || ntohs(udp->dest) == 53)
+                    {
+                        std::cout << "DNS packet detected" << std::endl;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
 
 // Checksum function
 unsigned short checksum(void *b, int len)
